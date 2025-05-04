d2682a32451e829d7db6b781f6c4b9ba7eb79f9d
Angira Bag <surabhibag9@gmail.com>
2025-05-04 15:35:17 +0530

    Update citi_forgot.php

--- Garbage_management_system/citizen/citi_forgot.php
+++ Garbage_management_system/citizen/citi_forgot.php
@@ -22,13 +22,13 @@ function sendMail($email, $reset_token){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'username';                     //SMTP username
+        $mail->Username   = 'emailid';                     //SMTP username
         $mail->Password   = 'password';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
         //Recipients
-        $mail->setFrom('projectpurpose010101@gmail.com', 'Waste Heroes');
+        $mail->setFrom('emailid', 'Waste Heroes');
         $mail->addAddress($email);     //Add a recipient
         
         //Content
